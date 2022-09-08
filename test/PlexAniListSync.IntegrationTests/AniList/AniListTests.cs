1cc0d0be9658863d9708430d5bd4141e42d1c764
Faust <Faustvii@users.noreply.github.com>
2022-09-08 17:28:50 +0200

    test: added first integration test for anilist

---
+++ test/PlexAniListSync.IntegrationTests/AniList/AniListTests.cs
@@ -0,0 +1,33 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using PlexAniListSync.Models.AniList;
+using PlexAniListSync.Services.AniList;
+using Xunit;
+
+namespace PlexAniListSync.IntegrationTests.AniList;
+
+public class AniListTests
+{
+    [Theory]
+    [InlineData(108241, "Gleipnir", 1)]
+    public async Task CanRetrieveAniListIdByTitleOnAPI(int expectedId, string title, int season)
+    {
+        var anilistOptions = new AniListOptions
+        {
+            TestMode = true,
+            Users = new[]
+            {
+                new AniListOptions.AniListUser { PlexUsernames = new[] { "" }, Token = "" }
+            }
+        };
+        var options = Options.Create(anilistOptions);
+        var logger = Mock.Of<ILogger<AniListService>>();
+        var service = new AniListService(options, logger, new AniListNet.AniClient());
+
+        var anilistId = await service.FindShowAsync(title, season);
+
+        Assert.Equal(expectedId, anilistId);
+    }
+}
